c462c2b01b577dca9b8409bfc99a411dd5bc91e6
Dipu Kumar Mohanto <63247883+dipu626@users.noreply.github.com>
2021-02-08 23:26:15 +0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-import SketchController from "./SketchController";
+import SketchController from "./monitorControl";
 import github from "raw-loader!@fortawesome/fontawesome-free/svgs/brands/github.svg";
 
 document.getElementById("github").innerHTML = github;
